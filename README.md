8b4dcc25b501dbc3f0af9f3e2130e6469968a8fb
sacredgeometry <lostrecordsuk@gmail.com>
2017-04-01 20:32:08 +0100

    Add empty swift project

---
+++ README.md
@@ -0,0 +1,6 @@
+# Salt v 0.0.1.1
+
+## Description
+
+Author: Brian Williams
+Website: A [www.brianjohnwilliams.com](http://brianjohnwilliams.com)
---
+++ Salt/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Salt
+//
+//  Created by Brian on 01/04/2017.
+//  Copyright © 2017 Iko. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
