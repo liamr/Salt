c4f286f18663ff0eaf9ab5ff6808e664fd6a89bc
sacredgeometry <lostrecordsuk@gmail.com>
2017-04-02 01:57:58 +0100

    add some more basic files for piano roll

---
+++ Salt/Note.swift
@@ -0,0 +1,16 @@
+//
+//  Note.swift
+//  Salt
+//
+//  Created by Brian on 02/04/2017.
+//  Copyright © 2017 Iko. All rights reserved.
+//
+
+import Foundation
+
+class Note {
+    var value: Int = 0;
+    var velocity: Double = 1.0;
+    var start: Double = 0.0;
+    var length: Double = 1.0;
+}
---
+++ Salt/NoteViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NoteViewController.swift
+//  Salt
+//
+//  Created by Brian on 02/04/2017.
+//  Copyright © 2017 Iko. All rights reserved.
+//
+
+import Foundation
+
+class NoteViewController: NSViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override var representedObject: Any? {
+        didSet {
+            // Update the view, if already loaded.
+        }
+    }
+    
+    
+}
