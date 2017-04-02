32a7368c73ae253009cd5cc027df01c87f5f6b29
sacredgeometry <lostrecordsuk@gmail.com>
2017-04-02 01:35:44 +0100

    Add pianorollgridview boiler plate

---
+++ Salt/PianoRollGridView.swift
@@ -0,0 +1,20 @@
+//
+//  PianoRoll.swift
+//  Salt
+//
+//  Created by Brian on 02/04/2017.
+//  Copyright © 2017 Iko. All rights reserved.
+//
+
+import Cocoa
+
+@IBDesignable
+class PianoRollGridView: NSView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+
+    }
+    
+}
