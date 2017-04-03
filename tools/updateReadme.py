874c3d67116abb383a1d333cc2f2219cdc1388e0
sacredgeometry <lostrecordsuk@gmail.com>
2017-04-03 01:25:35 +0100

    Fix formatting on header in readme

--- tools/updateReadme.py
+++ tools/updateReadme.py
@@ -7,7 +7,7 @@ def main():
 	with open(readmePath, 'r') as file:
 		data = file.readlines()
 
-	data[0] = "Salt {0}.{1} \n".format(versionNumber, buildNumber)
+	data[0] = "# Salt {0}.{1} \n".format(versionNumber, buildNumber)
 
 	with open(readmePath, 'w') as file:
 		file.writelines(data)
